3cc62c3c65ee4766fa0bb5f0bdb915f8ebee4f83
qwarl <quannngoc98@gmail.com>
2022-09-27 09:45:58 +0700

    add MBG to quotation

--- src/app/models/QuotationLog.js
+++ src/app/models/QuotationLog.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Quotation = new Schema({
+    code: {
+        type: String,
+        // required: true
+    },
     pol: {
         type: String,
         // required: true
