eb0c7efdee8c35591d1979edf386198ffcb8ba7d
huuthang2000 <huuthang13688@gmail.com>
2022-12-09 14:45:41 +0700

    done ops

--- src/app/controllers/LogDeparment/AdvanceOPSController.js
+++ src/app/controllers/LogDeparment/AdvanceOPSController.js
@@ -1,19 +1,25 @@
-const AdvanceOPS = require("../../models/LogDeparment/AdvanceOPS");
+const AdvanceOPS = require('../../models/LogDeparment/AdvanceOPS')
 
 class AdvanceOPSController {
   //[GET] /AdvanceOPSs/getAll
   async getAll(req, res) {
     try {
-      const advanceOPS = await AdvanceOPS.find({});
+      const OPS = await AdvanceOPS.find({})
+      // console.log(OPS.length);
+      // for (let i = 0; i < OPS.length; i++) {
+      //   const advanceOPS = OPS[i].ops;
+      //   // console.log("Hien Thị" + advanceOPS);
+      // }
+      const advanceOPS = OPS[0].ops
       res.status(200).json({
         success: true,
-        message: "Get all AdvanceOPS  successfully",
+        message: 'Get all AdvanceOPS  successfully',
         advanceOPS,
-      });
-      console.log("Get all AdvanceOPS successfully");
+      })
+      console.log('Get all AdvanceOPS successfully')
     } catch (error) {
-      console.log(error);
-      res.status(400).json({ success: false, message: "Get failed" });
+      console.log(error)
+      res.status(400).json({ success: false, message: 'Get failed' })
     }
   }
 
@@ -21,16 +27,16 @@ class AdvanceOPSController {
     try {
       const AdvanceOPS = await AdvanceOPS.find({
         userCreate: req.params.name,
-      });
+      })
       res.status(200).json({
         success: true,
-        message: "Get all AdvanceOPS log successfully",
+        message: 'Get all AdvanceOPS log successfully',
         AdvanceOPS,
-      });
-      console.log("Get all AdvanceOPS log successfully");
+      })
+      console.log('Get all AdvanceOPS log successfully')
     } catch (error) {
-      console.log(error);
-      res.status(400).json({ success: false, message: "Get failed" });
+      console.log(error)
+      res.status(400).json({ success: false, message: 'Get failed' })
     }
   }
 
@@ -43,16 +49,16 @@ class AdvanceOPSController {
         userCreate: req.body.userCreate,
         creacteAt: req.body.creacteAt,
         updateAt: req.body.updateAt,
-      });
-      await newAdvanceOPS.save();
+      })
+      await newAdvanceOPS.save()
       res.status(200).json({
         success: true,
-        message: "Create new AdvanceOPS successfully",
+        message: 'Create new AdvanceOPS successfully',
         newAdvanceOPS,
-      });
+      })
     } catch (error) {
-      console.log(error);
-      res.status(400).json({ success: false, message: "Create failed" });
+      console.log(error)
+      res.status(400).json({ success: false, message: 'Create failed' })
     }
   }
 
@@ -62,24 +68,24 @@ class AdvanceOPSController {
       const updateAdvanceOPS = await AdvanceOPS.findByIdAndUpdate(
         req.params._id,
         req.body,
-        { new: true }
-      );
+        { new: true },
+      )
       res.status(200).json({
         success: true,
-        message: "Update phong log successfully",
+        message: 'Update phong log successfully',
         updateAdvanceOPS,
-      });
-      console.log("Update phong log successfully");
+      })
+      console.log('Update phong log successfully')
     } catch (error) {
-      console.log(error);
-      res.status(400).json({ success: false, message: "Update failed" });
+      console.log(error)
+      res.status(400).json({ success: false, message: 'Update failed' })
     }
   }
 
   //[GET] /AdvanceOPSs/search
   async search(req, res) {
     try {
-      const { pol, pod, month, freight } = req.query;
+      const { pol, pod, month, freight } = req.query
       // console.log(pol, pod, type, month, continent);
 
       //code sua
@@ -87,30 +93,30 @@ class AdvanceOPSController {
       if (pol && month && freight) {
         const AdvanceOPSs = await AdvanceOPS.find({
           $and: [{ pol: pol }, { month: month }, { freight: freight }],
-        });
+        })
         return res.status(200).json({
           success: true,
-          message: "Search phong log successfully",
+          message: 'Search phong log successfully',
           AdvanceOPSs,
-        });
-        console.log("Search phong log successfully");
+        })
+        console.log('Search phong log successfully')
       }
       if (pod && month && freight) {
         const AdvanceOPSs = await AdvanceOPS.find({
           $and: [{ pod: pod }, { month: month }, { freight: freight }],
-        });
+        })
         return res.status(200).json({
           success: true,
-          message: "Search phong log successfully",
+          message: 'Search phong log successfully',
           AdvanceOPSs,
-        });
-        console.log("Search phong log successfully");
+        })
+        console.log('Search phong log successfully')
       }
     } catch (error) {
-      console.log(error);
-      return res.status(400).json({ success: false, message: "Search failed" });
+      console.log(error)
+      return res.status(400).json({ success: false, message: 'Search failed' })
     }
   }
 }
 
-module.exports = new AdvanceOPSController();
+module.exports = new AdvanceOPSController()
--- src/app/models/LogDeparment/AdvanceOPS.js
+++ src/app/models/LogDeparment/AdvanceOPS.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const AdvanceOPS = new Schema(
   {
-    ops: [Array],
+    ops: [],
     userUpdate: String,
     userCreate: String,
     creacteAt: String,
     updateAt: String,
   },
-  { timestamps: true, versionKey: false }
-);
-module.exports = mongoose.model("AdvanceOPS", AdvanceOPS, "AdvanceOPS");
+  { timestamps: true, versionKey: false },
+)
+module.exports = mongoose.model('AdvanceOPS', AdvanceOPS, 'AdvanceOPS')
